04b6f11bc3782351d77418c7d8ca82302219b896
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-25 02:35:51 -0400

    Added newuserform - working with backend, form validation and session logic needed

--- immunization-tracker/src/User/Login.js
+++ immunization-tracker/src/User/Login.js
@@ -6,15 +6,21 @@ import axios from "axios";
 function Login(props) {
   return (
     <Form>
+      {props.incorrectCredentials && <h1 className="loginError">Incorrect Credentials</h1>}
       <h1>Login</h1>
-      <div className="loginError">
-        {props.errors.email && <h3>{props.errors.email}</h3>}
-      </div>
+
+        <div className="loginError">
+          {props.errors.email && <h3>{props.errors.email}</h3>}
+        </div>
+
       <Field type="text" name="email" placeholder="Email" />
-      <div className="loginError">
-        {props.errors.password && <h3>{props.errors.password}</h3>}
-      </div>
+
+        <div className="loginError">
+          {props.errors.password && <h3>{props.errors.password}</h3>}
+        </div>
+
       <Field type="password" name="password" placeholder="Password" />
+
       <button type="submit">Submit</button>
     </Form>
   );
@@ -32,7 +38,8 @@ const FormikLogin = withFormik({
     email: Yup.string()
       .email()
       .required(),
-    password: Yup.string().required()
+    password: Yup.string()
+      .required()
   }),
 
   handleSubmit(loginData) {
@@ -41,7 +48,10 @@ const FormikLogin = withFormik({
         "https://bw4-immunization.herokuapp.com/api/parents/login",
         loginData
       )
-      .then(res => console.log(JSON.stringify(res)));
+      .then(res => {
+        console.log(JSON.stringify(res.status));
+      })
+      .catch(err => console.log(err, '!!!'))
   }
 })(Login);
 
---
+++ immunization-tracker/src/User/NewUserForm.js
@@ -0,0 +1,84 @@
+import React, {useState, useEffect} from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const UserForm = (props) => {
+	// const [users, setUsers] = useState ([])
+	// const [numDependents, setNumDependents] = useState (0)
+
+    // useEffect (()=> {
+    //     if (status) {
+    //     setUsers([...users, status])
+    //     }
+    // }, [status])
+    
+	
+	return (
+		<Form>
+		<h2>Basic Contact Information</h2>
+		<p>Your Name: </p>
+        
+		<Field type ="text" name ="firstname" placeholder ="*First Name" />
+		
+		<Field type ="text" name ="lastname" placeholder ="*Last Name" />	
+		
+		<p>Date of Birth: </p>
+		
+		<Field type ="date" name ="dob" placeholder ="*Month/Date/Year" />	
+	
+
+		<p>Your Address</p>
+		
+		<Field type ="text" name ="street" placeholder ="*Street Address" />
+		
+		<Field type ="text" name ="city" placeholder ="*City" />
+		
+		<Field type ="text" name ="state_province" placeholder ="*State/Province" />
+
+		<p>Contact Information</p>
+		
+		<Field type ="text" name ="phonenumber" placeholder ="*Phone Number" />
+
+		<h2>Set Email Address and Password</h2>
+		<p>Email Address</p>
+        <Field type ="email" name ="email" placeholder ="*Email Address (Primary)" />
+
+		<p>Password</p>
+        <Field type ="password" name ="password" placeholder ="*Password" />
+		
+        <button type ="submit"> + Sign Me Up </button>
+
+		</Form>
+	);
+};
+
+export default withFormik({
+	mapPropsToValues({firstname, lastname, dob, street, city, state_province, phonenumber, childName, kidBirth, email, password, permission}){
+		return{
+			firstname: firstname || "", 
+			lastname: lastname || "", 
+			dob: dob || "",
+            
+			street: street || "",
+			city: city || "",
+			state_province: state_province || "",
+			phonenumber: phonenumber || "",
+		
+			email: email || "",
+            password: password ||"",
+        }
+    },
+    handleSubmit(values, FormikBag){
+       axios.post("https://bw4-immunization.herokuapp.com/api/parents/register", values)
+        .then((res) => {
+            console.log('success', res);
+            FormikBag.props.history.push('/home')
+            // Reroute to landing page
+        })
+        .catch((err) => {
+            console.log("Error:", err);
+            // Error message
+        });
+    }
+})(UserForm);
